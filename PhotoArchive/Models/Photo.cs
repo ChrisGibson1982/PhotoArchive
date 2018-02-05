68bee25a38de263eb2027f96f876ebd3b1aaa5d7
Chris Gibson <cgfootman@hotmail.com>
2018-02-05 10:33:46 +0000

    Add Photo Model

---
+++ PhotoArchive/Models/Photo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhotoArchive.Models
+{
+    public class Photo
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Notes { get; set; }
+        public string Location { get; set; }
+        public string FilePath { get; set; }
+        public DateTime DateAdded { get; set; }
+
+
+    }
+}
